29f7f635e117660bd9bcc25fd9b7de1891e573f2
Gerardo Cordero <cordero.gerard@gmail.com>
2019-01-13 22:42:36 -0500

    Create the initial form to setup the calendar.
    working in country codes.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,67 @@
-// Set up your application entry point here...
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './styles/app.scss';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: null,
+      workDays: 0,
+      countryCode: []
+    };
+
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleWorkDaysChange = this.handleWorkDaysChange.bind(this);
+    this.showCalendar = this.showCalendar.bind(this);
+  }
+
+  handleDateChange(e){
+    this.setState({startDate: e.target.value})
+  }
+
+  handleWorkDaysChange(e){
+    this.setState({startDate: e.target.value})
+  }
+
+  showCalendar(){
+    console.log("click showCalendar");
+  }
+
+  render() {
+    let localDateFunction = () => {
+      let local = new Date();
+      local.setMinutes(local.getMinutes() - local.getTimezoneOffset());
+      return local.toJSON().slice(0,10);
+    };
+    return (
+      <div className='App'>
+        <h1>Tekton Labs WorkDays and Holidays Calendar in React</h1>
+        <div className="MainContainer">
+          <div className="StartDate">
+            <input type="date"
+                   id="startDate"
+                   value={
+                     (this.state.startDate == null) ? localDateFunction() : this.state.startDate
+                   }
+                   onChange={this.handleDateChange}/>
+          </div>
+          <div className="Days">
+            <input type="text"/>
+          </div>
+          <div className="CountryCode">
+            <select name="countryCode" id="countryCode">
+              <option value="select country">Select Country Code</option>
+            </select>
+          </div>
+          <button onClick={this.showCalendar}>Submit</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App/>,
+  document.querySelector('#app')
+);
